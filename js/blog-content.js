c0fe66aa3550d8339d586170a97a595be17c2e8a
tatsel <asiptsov@gmail.com>
2014-12-11 19:24:10 +0300

    fix gallery from json

--- js/blog-content.js
+++ js/blog-content.js
@@ -2,18 +2,17 @@
 template = '{{#.}}<p><a href="#" data-toggle="modal" data-target="#myModal" onclick="loadSingle(\'{{header}}\')"><span class="like-a-h">{{header}}</span></a><br><br>added by: <strong>{{author}}</strong> on <strong>{{date}}</strong><br><br>{{text}}<hr></p>{{/.}}';
 withoutPaginTemplate = '{{#.}}<p><span class="like-a-h">{{header}}</span><br><br>added by: <strong>{{author}}</strong> on <strong>{{date}}</strong><br><br>{{text}}<hr></p>{{/.}}';
 templateSingle = '{{#.}}<p><a href="#" data-toggle="modal" data-target="#myModal" onclick="loadSingle(\'{{header}}\')"><h6>{{header}}</h6></a><p>added on {{date}}</p><hr>{{/.}}';
-sorryTemplate = '{{#.}}<center><h3>{{sorry}}</h3><br><br><p>{{text}}</p>{{/.}}';
-imgTemplate = '{{#.}}<a href="{{href}}" title="{{title}}" data-gallery><img src="{{src}}"></img></a>';
+sorryTemplate = '{{#.}}<h3>{{sorry}}</h3><br><br><p>{{text}}</p>{{/.}}';
+imgTemplate = '{{#.}}<a href="{{href}}" title="{{title}}" data-gallery><img src="{{src}}"></a>{{/.}}';
 
 function loadTemplate() {			 
 	$.getJSON('js/json/content.json').done(function(jsondata) {
+		$('#links')[0].innerHTML = Mustache.render(imgTemplate, jsondata.imgs);
 		$('#content-container')[0].innerHTML = Mustache.render(template, jsondata.articles);
 		pagin();
 		for(var i=0; i<jsondata.articles.length || i<5; i++){
-			var obj = jsondata.articles[i];	  
 			$('#last-headers-container'+i)[0].innerHTML = Mustache.render(templateSingle, jsondata.articles[i]);
 		};
-		$('#links')[0].innerHTML = Mustache.render(imgTemplate, jsondata.images);		   
 	});
 }
 
